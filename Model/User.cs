d8fe076a097b8864a0e98f8bc0ba96323ad78595
Cole McFarland <cmcfarland@homesteaderslife.com>
2020-02-27 09:20:42 -0600

    updating for HLC demo

---
+++ Model/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Pwronco.Validation;
+
+namespace Pwronco.Model
+{
+    public class User
+    {
+       
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string SecondName { get; set; }
+
+            private string _contactType = "-1";
+            public string ContactType
+            {
+                get
+                {
+                    return _contactType;
+                }
+                set
+                {
+                    _contactType = value;
+                }
+            }
+
+            public string Email { get; set; }
+            public string Phone { get; set; }
+        
+    }
+}
---
+++ Model/UserFormModel.cs
@@ -0,0 +1,24 @@
+﻿using Pwronco.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pwronco.Model
+{
+   
+        public class UserFormModel
+        {
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+            public string ContactType { get; set; }
+            [DynamicContactValidation(ParentFieldName: "ContactType", FieldType: "Email", ValidationTypes: new[] { "required", "email" })]
+            public string Email { get; set; }
+            [DynamicContactValidation(ParentFieldName: "ContactType", FieldType: "Phone", ValidationTypes: new[] { "required", "phone" })]
+            public string Phone { get; set; }
+        }
+    
+}
